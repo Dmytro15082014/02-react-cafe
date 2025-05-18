5e06a76b370be03902ce995f39d19137dbba7130
Dmytro Teren <leo20091986@gmail.com>
2025-05-18 14:00:26 +0300

    add total & positive rate

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,14 +8,19 @@ import VoteStats from "../VoteStats/VoteStats";
 export default function App() {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
   const [canReset, setCanReset] = useState(true);
+
   const handleVote = (type: VoteType) => {
     setVotes({ ...votes, [type]: votes[type] + 1 });
   };
+
   const resetVotes = () => {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   };
+
   const showReset = () => setCanReset(!canReset);
 
+  const total = Object.values(votes).reduce((i, vote) => i + vote, 0);
+  const positive = total ? Math.round((votes.good / total) * 100) : 0;
   return (
     <div className={css.app}>
       <CafeInfo />
@@ -24,7 +29,7 @@ export default function App() {
         onReset={resetVotes}
         canReset={showReset}
       />
-      <VoteStats votes={votes} totalVotes={0} positiveRate={0} />
+      <VoteStats votes={votes} totalVotes={total} positiveRate={positive} />
     </div>
   );
 }
